71a3ea5bda0e26cdf65387dc68953e39a80595e1
Jason Sinn <jrs341@me.com>
2016-08-21 13:12:24 -0500

    game stops, sort of

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -1,14 +1,18 @@
-var numGuessRem = 6;
+
 
 var guessedLetters = [];
 
-var hairbands = ["poison"/*, "van halen", "aero smith", "white snake"*/];
+var hairbands = ["poison", "van halen", "aero smith", "white snake"];
 
 var arr = hairbands[Math.floor(Math.random() * hairbands.length)];
 
 var blank = new Array(arr.lenght);
 
-// var blankArr = arr.split("");
+var numGuessRem = arr.length + 3;
+
+var wins = 0;
+
+var continueGame = true;
 
 for (var i = 0; i < arr.length; i++) {
 	blank[i] = '_';
@@ -30,7 +34,7 @@ function include(arr, obj) {
 			guessedLetters.push(obj);
 		}
 
-		if (arr.indexOf(obj) === -1 && numGuessRem >= 1) {
+		if (arr.indexOf(obj) === -1 && numGuessRem >= 1 && arr.split("").toString() != blank.join()) {
 			console.log('try again');	
 		}
 
@@ -46,6 +50,8 @@ function arraysEqual(arr1, arr2) {
 
 		if (arr1.split("").toString() == arr2.join()) {
 			console.log('You Win');
+			wins ++;
+			return continueGame = false;
 		} else {
 			console.log('Keep Going');
 		}	
@@ -54,15 +60,17 @@ function arraysEqual(arr1, arr2) {
 
 	console.log(arr);
 
-	console.log(blank.toString());
+	console.log(blank);
 
-	console.log(arr.split("").toString());
+	console.log(numGuessRem);
 
-	console.log(blank.join());
+	console.log(wins);
 
 	document.onkeyup = function(event) {
 
 		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+		if (continueGame) {
 
 			if(numGuessRem >= 1) {
 
@@ -81,6 +89,7 @@ function arraysEqual(arr1, arr2) {
 			console.log('Game Over');
 
 			}
+		}
 
 		console.log(arr);
 
@@ -89,5 +98,7 @@ function arraysEqual(arr1, arr2) {
 		console.log(guessedLetters);
 
 		console.log(numGuessRem);
+
+		console.log(wins);
 	}
 
